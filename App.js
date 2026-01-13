8770f2a415ad0c42e51ea19dce6c2955f27fb831
Zhe <hwazhezhi@gmail.com>
2026-01-13 10:57:32 +0800

    commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { FlatList, Alert, TouchableOpacity, StatusBar, Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
-export default function App() {
+let originalData = [];
+
+const App = () => {
+  const [mydata, setMydata] = useState([]);
+
+  const myurl = "https://onlinecardappwebservice-kb7a.onrender.com/allcomics"
+  
+  useEffect(() => {
+    fetch(myurl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        if(originalData.length<1) {
+          setMydata(myJson);
+          originalData = myJson;
+        }
+      })
+  }, []);
+
+  const FilterData = (text) => {
+    if(text!='') {
+      let myFilteredData = originalData.filter((item)=>
+        item.comic_name.includes(text));
+      setMydata(myFilteredData);
+    }
+    else {
+      setMydata(originalData);
+    }
+  }
+
+  const renderItem = ({item, index}) => {
+    return (
+    
+    <TouchableOpacity onPress={() => Alert.alert('You selected '+item.comic_name)}>
+    <View style={styles.row}>
+    <Text style={styles.title}>{item.comic_name}
+    </Text>
+
+    <Image source={{uri: item.comic_pic}} style={{ width: 130, height: 170, resizeMode: 'contain' }} />
+    </View>
+    </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.view}>
+      <StatusBar/>
+      <Text style={styles.text}>Search:</Text>
+      <TextInput style={{borderWidth:3, marginBottom: 15}} onChangeText={(text)=>{FilterData(text)}} />
+      <Text style={styles.header}>Comics</Text>
+      <FlatList data={mydata} renderItem={renderItem} style={{backgroundColor: 'yellow'}} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    fontWeight: 'bold',
+    marginBottom: 10
   },
+  view: {
+    marginTop: 20,
+    margin: 10
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderWidth: 1
+    
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15,
+    fontSize: 25
+  },
+  title: {
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 16
+  }
 });
+
+export default App;
